016bf080dda0dc1b5a5230a44e61ad6880320fc9
tranvanmod <107925111+tranvanmod@users.noreply.github.com>
2024-06-24 17:26:02 +0700

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,60 @@
+using System;
+using HitClub.DatabaseManager;
+using Serilog;
+using Serilog.Events;
+
+namespace HitClub.Logging
+{
+    public class ServerLogger : IServerLogger
+    {
+        private readonly ILogger _logger;
+
+        public ServerLogger()
+        {
+            _logger = new LoggerConfiguration()
+                .WriteTo.Console(outputTemplate: "{Message:lj}{NewLine}{Exception}")
+                .WriteTo.File("logging/log-.txt", LogEventLevel.Error, rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+        }
+        
+        public interface IServerLogger
+        {
+        void Debug(string message);
+        void Print(string message);
+        void Info(string info);
+        void Warning(string message, Exception exception);
+        void Error(string message, Exception exception);
+        void Error(string message);
+        }
+
+        public void Debug(string message)
+        {
+            if(Manager.gI().IsDebug) _logger.Information($"DEBUG ==> {message}");
+        }
+        
+        public void Print(string message)
+        {
+            _logger.Information($"==> {message}");
+        }
+
+        public void Info(string info)
+        {
+            _logger.Information(info);
+        }
+
+        public void Warning(string message, Exception exception = null)
+        {
+            _logger.Warning(message, exception);
+        }
+
+        public void Error(string message, Exception exception)
+        {
+            _logger.Error(message, exception);
+        }
+
+        public void Error(string message)
+        {
+            _logger.Error(message);
+        }
+    }
+}
